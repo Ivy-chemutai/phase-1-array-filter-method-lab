3d2792b440985fe859bd77a6a018720887b9fc90
ivy <ivy.chemutai@student.moringaschool.com>
2025-06-13 21:43:13 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, value) {
+    return array.filter(item => item.toLowerCase() === value.toLowerCase());
+
+}
+function fuzzyMatch(array, value) {
+    return array.filter(item => item.slice(0, value.length) === value);
+}
+function matchName(array, value) {
+    return array.filter(item => item.name === value);
+}
+module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+};
